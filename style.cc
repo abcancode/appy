bb8c79a3277db5ee0e3580ce4dd5083da44d352b
Ugwu Abraham Chidozie <jonasbraham@gmail.com>
2020-03-17 23:41:55 +0100

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,54 @@
+
+      
+      .jumbotron {
+          
+          background-image: url(bg.jpg);
+          text-align: center;
+          margin-top: 50px;
+          background-repeat: no-repeat;
+          background-size: cover;
+          color: #fff;
+      }
+      
+      #email {
+          
+          width: 300px;
+          
+      }
+      
+      #appSummary {
+          
+          text-align: center;
+          margin-top:50px;
+          margin-bottom: 50px;
+          
+      }
+      
+      .card-img-top {
+          
+          width: 100%;
+          
+      }
+      
+      #appStoreIcon {
+          
+          width: 200px;
+          
+      }
+      
+      #footer {
+          
+          background-color: rgb(0,206,209);
+          padding-top: 150px;
+          margin-top: 50px;
+          text-align: center;
+          padding-bottom: 150px;
+      }
+      
+      body {
+          
+          position: relative;
+          font-family:  'Poppins', 'sans-serif' ;
+          
+      }
+  
